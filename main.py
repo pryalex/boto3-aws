3fbd92f2f693b4fcc446554f8aa6dd725595b93f
pryalex <prihodko@granatum.solutions>
2023-02-21 21:41:17 +0200

    add

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import boto3
 import os
-aws_access_key_id = os.environ['']
-aws_secret_access_key = os.environ['']
-aws_region = os.environ['']
+aws_access_key_id = os.environ['aws_access_key_id']
+aws_secret_access_key = os.environ['aws_secret_access_key']
+aws_region = os.environ['aws_region']
 ec2 = boto3.resource('ec2')
 client = boto3.client('ec2')
 client_elb = boto3.client('elbv2')
